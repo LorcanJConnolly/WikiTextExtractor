0e21e5bfad2d4c991135373fba7ba26ec5b599db
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2024-01-21 17:08:15 +0000

    Started a space for Wikipidea robot.txt rules to be stored to be used to create a nice bot.

---
+++ Wiki_robots_txt.py
@@ -0,0 +1,175 @@
+
+"""
+Disallow: /w/
+Disallow: /api/
+Disallow: /trap/
+Disallow: /wiki/Special:
+Disallow: /wiki/Spezial:
+Disallow: /wiki/Spesial:
+Disallow: /wiki/Special%3A
+Disallow: /wiki/Spezial%3A
+Disallow: /wiki/Spesial%3A
+
+Disallow: /wiki/%D8%AE%D8%A7%D8%B5:Search
+Disallow: /wiki/%D8%AE%D8%A7%D8%B5%3ASearch
+
+Disallow: /wiki/Wikipedia:L%C3%B6schkandidaten/
+Disallow: /wiki/Wikipedia:Löschkandidaten/
+Disallow: /wiki/Wikipedia:Vandalensperrung/
+Disallow: /wiki/Wikipedia:Benutzersperrung/
+Disallow: /wiki/Wikipedia:Vermittlungsausschuss/
+Disallow: /wiki/Wikipedia:Administratoren/Probleme/
+Disallow: /wiki/Wikipedia:Adminkandidaturen/
+Disallow: /wiki/Wikipedia:Qualitätssicherung/
+Disallow: /wiki/Wikipedia:Qualit%C3%A4tssicherung/
+
+Disallow: /wiki/Wikipedia:Vandalismusmeldung/
+Disallow: /wiki/Wikipedia:Gesperrte_Lemmata/
+Disallow: /wiki/Wikipedia:Löschprüfung/
+Disallow: /wiki/Wikipedia:L%C3%B6schprüfung/
+Disallow: /wiki/Wikipedia:Administratoren/Notizen/
+Disallow: /wiki/Wikipedia:Schiedsgericht/Anfragen/
+Disallow: /wiki/Wikipedia:L%C3%B6schpr%C3%BCfung/
+
+
+Disallow: /wiki/Wikipedia:Checkuser/
+Disallow: /wiki/Wikipedia_Diskussion:Checkuser/
+Disallow: /wiki/Wikipedia_Diskussion:Adminkandidaturen/
+
+Disallow: /wiki/Wikipedia:Spam-Blacklist-Log
+Disallow: /wiki/Wikipedia%3ASpam-Blacklist-Log
+Disallow: /wiki/Wikipedia_Diskussion:Spam-Blacklist-Log
+Disallow: /wiki/Wikipedia_Diskussion%3ASpam-Blacklist-Log
+
+Disallow: /wiki/Wikipedia:Articles_for_deletion/
+Disallow: /wiki/Wikipedia%3AArticles_for_deletion/
+Disallow: /wiki/Wikipedia:Votes_for_deletion/
+Disallow: /wiki/Wikipedia%3AVotes_for_deletion/
+Disallow: /wiki/Wikipedia:Pages_for_deletion/
+Disallow: /wiki/Wikipedia%3APages_for_deletion/
+Disallow: /wiki/Wikipedia:Miscellany_for_deletion/
+Disallow: /wiki/Wikipedia%3AMiscellany_for_deletion/
+Disallow: /wiki/Wikipedia:Miscellaneous_deletion/
+Disallow: /wiki/Wikipedia%3AMiscellaneous_deletion/
+Disallow: /wiki/Wikipedia:Copyright_problems
+Disallow: /wiki/Wikipedia%3ACopyright_problems
+Disallow: /wiki/Wikipedia:Protected_titles/
+Disallow: /wiki/Wikipedia%3AProtected_titles/
+
+Disallow: /wiki/Wikipedia:WikiProject_Spam/
+Disallow: /wiki/Wikipedia%3AWikiProject_Spam/
+
+Disallow: /wiki/MediaWiki:Spam-blacklist
+Disallow: /wiki/MediaWiki%3ASpam-blacklist
+Disallow: /wiki/MediaWiki_talk:Spam-blacklist
+Disallow: /wiki/MediaWiki_talk%3ASpam-blacklist
+
+Disallow: /wiki/Wikipedia:Requests_for_arbitration/
+Disallow: /wiki/Wikipedia%3ARequests_for_arbitration/
+Disallow: /wiki/Wikipedia:Requests_for_comment/
+Disallow: /wiki/Wikipedia%3ARequests_for_comment/
+Disallow: /wiki/Wikipedia:Requests_for_adminship/
+Disallow: /wiki/Wikipedia%3ARequests_for_adminship/
+
+Disallow: /wiki/Wikipedia_talk:Articles_for_deletion/
+Disallow: /wiki/Wikipedia_talk%3AArticles_for_deletion/
+Disallow: /wiki/Wikipedia_talk:Votes_for_deletion/
+Disallow: /wiki/Wikipedia_talk%3AVotes_for_deletion/
+Disallow: /wiki/Wikipedia_talk:Pages_for_deletion/
+Disallow: /wiki/Wikipedia_talk%3APages_for_deletion/
+Disallow: /wiki/Wikipedia_talk:Miscellany_for_deletion/
+Disallow: /wiki/Wikipedia_talk%3AMiscellany_for_deletion/
+Disallow: /wiki/Wikipedia_talk:Miscellaneous_deletion/
+Disallow: /wiki/Wikipedia_talk%3AMiscellaneous_deletion/
+
+Disallow: /wiki/Wikipedia:Changing_username
+Disallow: /wiki/Wikipedia%3AChanging_username
+Disallow: /wiki/Wikipedia:Changing_username/
+Disallow: /wiki/Wikipedia%3AChanging_username/
+Disallow: /wiki/Wikipedia_talk:Changing_username
+Disallow: /wiki/Wikipedia_talk%3AChanging_username
+Disallow: /wiki/Wikipedia_talk:Changing_username/
+Disallow: /wiki/Wikipedia_talk%3AChanging_username/
+
+Disallow: /wiki/Wikipedia:Poistettavat_sivut
+Disallow: /wiki/K%C3%A4ytt%C3%A4j%C3%A4:
+Disallow: /wiki/Käyttäjä:
+Disallow: /wiki/Keskustelu_k%C3%A4ytt%C3%A4j%C3%A4st%C3%A4:
+Disallow: /wiki/Keskustelu_käyttäjästä:
+Disallow: /wiki/Wikipedia:Yll%C3%A4pit%C3%A4j%C3%A4t/
+Disallow: /wiki/Wikipedia:Ylläpitäjät/
+
+Disallow: /wiki/%D7%9E%D7%99%D7%95%D7%97%D7%93:Search
+Disallow: /wiki/%D7%9E%D7%99%D7%95%D7%97%D7%93%3ASearch
+
+Disallow: /wiki/ויקיפדיה:רשימת_מועמדים_למחיקה/
+Disallow: /wiki/ויקיפדיה%3Aרשימת_מועמדים_למחיקה/
+Disallow: /wiki/%D7%95%D7%99%D7%A7%D7%99%D7%A4%D7%93%D7%99%D7%94:%D7%A8%D7%A9%D7%99%D7%9E%D7%AA_%D7%9E%D7%95%D7%A2%D7%9E%D7%93%D7%99%D7%9D_%D7%9C%D7%9E%D7%97%D7%99%D7%A7%D7%94/
+Disallow: /wiki/%D7%95%D7%99%D7%A7%D7%99%D7%A4%D7%93%D7%99%D7%94%3A%D7%A8%D7%A9%D7%99%D7%9E%D7%AA_%D7%9E%D7%95%D7%A2%D7%9E%D7%93%D7%99%D7%9D_%D7%9C%D7%9E%D7%97%D7%99%D7%A7%D7%94/
+Disallow: /wiki/ויקיפדיה:ערכים_לא_קיימים_ומוגנים
+Disallow: /wiki/ויקיפדיה%3Aערכים_לא_קיימים_ומוגנים
+Disallow: /wiki/%D7%95%D7%99%D7%A7%D7%99%D7%A4%D7%93%D7%99%D7%94:%D7%A2%D7%A8%D7%9B%D7%99%D7%9D_%D7%9C%D7%90_%D7%A7%D7%99%D7%99%D7%9E%D7%99%D7%9D_%D7%95%D7%9E%D7%95%D7%92%D7%A0%D7%99%D7%9D
+Disallow: /wiki/%D7%95%D7%99%D7%A7%D7%99%D7%A4%D7%93%D7%99%D7%94%3A%D7%A2%D7%A8%D7%9B%D7%99%D7%9D_%D7%9C%D7%90_%D7%A7%D7%99%D7%99%D7%9E%D7%99%D7%9D_%D7%95%D7%9E%D7%95%D7%92%D7%A0%D7%99%D7%9D
+Disallow: /wiki/ויקיפדיה:דפים_לא_קיימים_ומוגנים
+Disallow: /wiki/ויקיפדיה%3Aדפים_לא_קיימים_ומוגנים
+Disallow: /wiki/%D7%95%D7%99%D7%A7%D7%99%D7%A4%D7%93%D7%99%D7%94:%D7%93%D7%A4%D7%99%D7%9D_%D7%9C%D7%90_%D7%A7%D7%99%D7%99%D7%9E%D7%99%D7%9D_%D7%95%D7%9E%D7%95%D7%92%D7%A0%D7%99%D7%9D
+Disallow: /wiki/%D7%95%D7%99%D7%A7%D7%99%D7%A4%D7%93%D7%99%D7%94%3A%D7%93%D7%A4%D7%99%D7%9D_%D7%9C%D7%90_%D7%A7%D7%99%D7%99%D7%9E%D7%99%D7%9D_%D7%95%D7%9E%D7%95%D7%92%D7%A0%D7%99%D7%9D
+
+Disallow: /wiki/Speci%C3%A1lis:Search
+Disallow: /wiki/Speci%C3%A1lis%3ASearch
+
+Disallow: /wiki/Wikipedia:Pagine_da_cancellare
+Disallow: /wiki/Wikipedia%3APagine_da_cancellare
+Disallow: /wiki/Wikipedia:Utenti_problematici
+Disallow: /wiki/Wikipedia%3AUtenti_problematici
+Disallow: /wiki/Wikipedia:Vandalismi_in_corso
+Disallow: /wiki/Wikipedia%3AVandalismi_in_corso
+Disallow: /wiki/Wikipedia:Amministratori
+Disallow: /wiki/Wikipedia%3AAmministratori
+Disallow: /wiki/Wikipedia:Proposte_di_cancellazione_semplificata
+Disallow: /wiki/Wikipedia%3AProposte_di_cancellazione_semplificata
+Disallow: /wiki/Categoria:Da_cancellare_subito
+Disallow: /wiki/Categoria%3ADa_cancellare_subito
+Disallow: /wiki/Wikipedia:Sospette_violazioni_di_copyright
+Disallow: /wiki/Wikipedia%3ASospette_violazioni_di_copyright
+Disallow: /wiki/Categoria:Da_controllare_per_copyright
+Disallow: /wiki/Categoria%3ADa_controllare_per_copyright
+Disallow: /wiki/Progetto:Rimozione_contributi_sospetti
+Disallow: /wiki/Progetto%3ARimozione_contributi_sospetti
+Disallow: /wiki/Categoria:Da_cancellare_subito_per_violazione_integrale_copyright
+Disallow: /wiki/Categoria%3ADa_cancellare_subito_per_violazione_integrale_copyright
+Disallow: /wiki/Progetto:Cococo
+Disallow: /wiki/Progetto%3ACococo
+Disallow: /wiki/Discussioni_progetto:Cococo
+Disallow: /wiki/Discussioni_progetto%3ACococo
+
+Disallow: /wiki/Wikipedia:Pagine_da_cancellare
+Disallow: /wiki/Wikipedia%3APagine_da_cancellare
+Disallow: /wiki/Wikipedia:Utenti_problematici
+Disallow: /wiki/Wikipedia%3AUtenti_problematici
+Disallow: /wiki/Wikipedia:Vandalismi_in_corso
+Disallow: /wiki/Wikipedia%3AVandalismi_in_corso
+Disallow: /wiki/Wikipedia:Amministratori
+Disallow: /wiki/Wikipedia%3AAmministratori
+Disallow: /wiki/Wikipedia:Proposte_di_cancellazione_semplificata
+Disallow: /wiki/Wikipedia%3AProposte_di_cancellazione_semplificata
+Disallow: /wiki/Categoria:Da_cancellare_subito
+Disallow: /wiki/Categoria%3ADa_cancellare_subito
+Disallow: /wiki/Wikipedia:Sospette_violazioni_di_copyright
+Disallow: /wiki/Wikipedia%3ASospette_violazioni_di_copyright
+Disallow: /wiki/Categoria:Da_controllare_per_copyright
+Disallow: /wiki/Categoria%3ADa_controllare_per_copyright
+Disallow: /wiki/Progetto:Rimozione_contributi_sospetti
+Disallow: /wiki/Progetto%3ARimozione_contributi_sospetti
+Disallow: /wiki/Categoria:Da_cancellare_subito_per_violazione_integrale_copyright
+Disallow: /wiki/Categoria%3ADa_cancellare_subito_per_violazione_integrale_copyright
+Disallow: /wiki/Progetto:Cococo
+Disallow: /wiki/Progetto%3ACococo
+Disallow: /wiki/Discussioni_progetto:Cococo
+Disallow: /wiki/Discussioni_progetto%3ACococo
+
+Disallow: /wiki/Wikipedia:%E5%89%8A%E9%99%A4%E4%BE%9D%E9%A0%BC/
+Disallow: /wiki/Wikipedia%3A%E5%89%8A%E9%99%A4%E4%BE%9D%E9%A0%BC/
+Disallow: /wiki/Wikipedia:%E5%88%A9%E7%94%A8%E8%80%85%E3%83%9A%E3%83%BC%E3%82%B8%E3%81%AE%E5%89%8A%E9%99%A4%E4%BE%9D%E9%A0%BC
+Disallow: /wiki/Wikipedia%3A%E5%88%A9%E7%94%A8%E8%80%85%E3%83%9A%E3%83%BC%E3%82%B8%E3%81%AE%E5%89%8A%E9%99%A4%E4%BE%9D%E9%A0%BC
+"""
