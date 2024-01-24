9a27c3f12f9e1ea3fef52be3e05f275c6dccae08
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2024-01-24 18:51:48 +0000

    Modified testing for valid and invalid user inputs.

--- Testing.py
+++ Testing.py
@@ -41,23 +41,16 @@ Later-Later
 
 
 class TestMain(unittest.TestCase):
-    # When the input is called, input return_value instead of waiting.
-    #
-    @patch('builtins.input', return_value=r"https://en.wikipedia.org/wiki/Main_Page")
-    def test_valid_user_root(self, mock_input):
-        self.assertEqual(get_user_root(), r"https://en.wikipedia.org/wiki/Main_Page")
+    """
+    For the functions requiring user inputs, invalid inputs do not return anything and the while loop continues.
+    In order to test for invalid user inputs, create a series of mock tests with invalid inputs and finish with a valid
+      input. We then test the valid scenario for the expected result.
+    """
 
-    @patch('builtins.input', side_effect=["", "https://www.wikihow.com/Main-Page", 7, False])
-    def test_invalid_user_root(self, mock_input):
-        expected_output = [
-            "\nThis not a valid Wikipedia URL.\n",
-            "\nThis not a valid Wikipedia URL.\n",
-            "\nThis not a valid Wikipedia URL.\n",
-            "\nThis not a valid Wikipedia URL.\n"
-        ]
-        for message in expected_output:
-            with self.subTest(message=message):
-                self.assertIn(message, "\nThis not a valid Wikipedia URL.\n")
+    @patch('builtins.input', side_effect=["", "https://www.wikihow.com/Main-Page", 7, False,
+                                               r"https://en.wikipedia.org/wiki/Main_Page"])
+    def test_get_user_root(self, mock_input):
+        self.assertEqual(get_user_root(), r"https://en.wikipedia.org/wiki/Main_Page")
 
     def test_is_not_wiki_url(self):
         invalid_urls = ["", "https://www.wikihow.com/Main-Page"]
@@ -70,6 +63,11 @@ class TestMain(unittest.TestCase):
         for url in valid_urls:
             with self.subTest(url=url):
                 self.assertTrue(is_wiki_url(url))
+
+    @patch('builtins.input', side_effect=[-5, 5, 4, -4, "", 3, 1, "", 5000, 10, "a", "b",
+                                          10, 10])
+    def test_invalid_journey(self, mock_input):
+        self.assertEqual(get_user_journey(), (10, 10))
 
 
 if __name__ == '__main__':
